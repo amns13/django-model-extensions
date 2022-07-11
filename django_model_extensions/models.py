52600672c7dfc356dc312b4d8ef4d4df79e963c0
amns13 <slimmsady@gmail.com>
2022-07-11 22:41:47 +0530

    Added Custom Manager for CraetedUpdatedTimestampModel for handling update queries

--- django_model_extensions/models.py
+++ django_model_extensions/models.py
@@ -1,5 +1,23 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+
+class CreateUpdateTimestampManager(models.Manager):
+    """
+    Custom Manager for CreatedUpdatedTimestampModel.
+    Used to update last_update_at on update.
+    """
+
+    def update(self, **kwargs: any) -> int:
+        """
+        Update values of fields in kwargs along with last_updated_at field.
+
+        Returns:
+            int: Number of records updated.
+        """
+        kwargs["last_updated_at"] = timezone.now()
+        return super().update(**kwargs)
 
 
 class CreatedUpdatedTimestampModel(models.Model):
@@ -10,6 +28,8 @@ class CreatedUpdatedTimestampModel(models.Model):
 
     created_at = models.DateTimeField(_("created timestamp"), auto_now_add=True)
     last_updated_at = models.DateTimeField(_("last update timestamp"), auto_now=True)
+
+    objects = CreateUpdateTimestampManager()
 
     class Meta:
         abstract = True
