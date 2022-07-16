5f65f5382be9da7bacbba0b6110793052532b5df
amns13 <slimmsady@gmail.com>
2022-07-16 18:47:39 +0530

    Added CreatedByUpdatedByModel

--- django_model_extensions/models.py
+++ django_model_extensions/models.py
@@ -1,5 +1,6 @@
-from typing import Iterable, Optional, Sequence
+from typing import Any, Collection, Iterable, Optional, Sequence
 
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -48,3 +49,64 @@ class CreatedUpdatedTimestampModel(models.Model):
     class Meta:
         abstract = True
         get_latest_by = "last_updated_at"
+
+
+class CreatedUpdatedByModel(models.Model):
+    """
+    An abstract base class that provides "created_by" and
+    "last_updated_by" fields.
+    These fields are not self managed and need to be handled
+    by the user of this model.
+    By default, these fields are None.
+    """
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name="%(app_label)s_%(class)ss_created",
+        related_query_name="%(app_label)s_%(class)s_created",
+        null=True,
+        blank=True,
+        verbose_name=_("created by"),
+        editable=False,
+    )
+    last_updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name="%(app_label)s_%(class)ss_last_updated",
+        related_query_name="%(app_label)s_%(class)s_last_updated",
+        null=True,
+        blank=True,
+        verbose_name=_("last updated by"),
+        editable=False,
+    )
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def from_db(
+        cls, db: Optional[str], field_names: Collection[str], values: Collection[Any]
+    ) -> "CreatedUpdatedByModel":
+        instance = super().from_db(db, field_names, values)
+        instance._old_values = dict(zip(field_names, values))
+        return instance
+
+    def save(self, *args, **kwargs) -> None:
+        if self.pk:
+            self.created_by_id = self._old_values["created_by_id"]
+        return super().save(*args, **kwargs)
+
+
+class CreatedUpdatedTimeUserModel(CreatedUpdatedTimestampModel, CreatedUpdatedByModel):
+    """
+    An abstract base class that provides "created_at", "last_updated_at",
+    "created_by", and "last_updated_by" fields.
+    "created_at", "last_updated_at" are self-managed fields.
+    "created_by", and "last_updated_by" are not self-managed and need to be handled
+    by the user of the model.
+    """
+
+    class Meta(CreatedUpdatedTimestampModel.Meta):
+        # Inherited from CreatedUpdatedTimestampModel.Meta for get_latest_by attribute
+        abstract = True
